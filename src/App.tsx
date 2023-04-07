617a57cd78f83f5efedef00fc85f4493069614c1
chiddy50 <codewelldev@gmail.com>
2023-04-08 00:08:43 +0100

    Merge changes from remote repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
-import './App.css'
-
 import MainLayout from './layouts/MainLayout';
 import Dashboard from './pages/Dashboard';
 import UserDetails from './pages/UserDetails';
